4df7c0faedd13d27423dc2b2a9121b641a4928fb
aarvisais <sukla.aarvisais2311@gmail.com>
2024-04-23 19:02:28 +0530

    new changes

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -528,3 +528,22 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
        
+
+
+
+
+// var btn = $('#button_scroll');
+
+// $(window).scroll(function() {
+//   if ($(window).scrollTop() > 300) {
+//     btn.addClass('show');
+//   } else {
+//     btn.removeClass('show');
+//   }
+// });
+
+// btn.on('click', function(e) {
+//   e.preventDefault();
+//   $('html, body').animate({scrollTop:0}, '300');
+// });
+
